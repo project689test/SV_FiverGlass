3c1ff0b38ceec8f7b5fc0e07860bbc30ccd3fb17
Dharmu Kumar <project.689.test@gmail.com>
2026-04-08 20:52:09 +0530

    Update Clients.jsx

--- client/src/components/clients/Clients.jsx
+++ client/src/components/clients/Clients.jsx
@@ -3,7 +3,7 @@ import { motion } from 'framer-motion';
 
 const Clients = () => {
   const clients = [
-    { logo: "../public/appu ghar.png", name: "Appu Ghar" },
+    { logo: ".C:\Users\dharm\Desktop\SV_FiverGlass\client\public\appu ghar.png", name: "Appu Ghar" },
     { logo: "client\public\hindustan.jpeg", name: "Hindustan" },
     { logo: "/public/gip.jpeg", name: "GIP" },
     { logo: "/public/logo1.png", name: "Client Logo" },
